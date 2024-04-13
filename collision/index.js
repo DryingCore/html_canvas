2e72406c1a6a4e696eca35db3304d880ef292264
DryingCore <lein0rb4g@gmail.com>
2024-04-13 14:57:24 -0300

    modified:   collision/index.js

--- collision/index.js
+++ collision/index.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas"); // gotcha canvas element
-const blockOne = canvas.getContext("2d"); // main character
+const context = canvas.getContext("2d"); // main character
 
 function main() {
 	// keymap consts
@@ -11,8 +11,14 @@ function main() {
 	const size = 50;
 	let posX = 50;
 	let posY = 50;
+	const collisionBlock = "#000"; // collider block color
 	const characterNormal = "#003"; // default character color
 	const characterCollision = "#f70"; // color if character do collision
+	let normalColor = characterNormal;
+
+	// character who will have collision logic
+	const collisionBlockX = canvas.width / 2 - size / 2;
+	const collisionBlockY = canvas.height / 2 - size / 2;
 
 	// key status
 	let isMoving = { up: false, down: false, left: false, right: false };
@@ -32,26 +38,37 @@ function main() {
 	}
 
 	function updateCharacterPosition() {
-		if (isMoving.up) posY -= 1;
-		if (isMoving.down) posY += 1;
-		if (isMoving.left) posX -= 1;
-		if (isMoving.right) posX += 1;
+		if (isMoving.up) posY -= 3;
+		if (isMoving.down) posY += 3;
+		if (isMoving.left) posX -= 3;
+		if (isMoving.right) posX += 3;
+	}
+
+	function collide() {
+		if (posX + size > collisionBlockX && posX < collisionBlockX + size && posY + size > collisionBlockY && posY < collisionBlockY + size) {
+			normalColor = characterCollision;
+		} else {
+			normalColor = characterNormal;
+		}
 	}
 
 	// render the main character
 	function renderCharacter() {
-		blockOne.fillStyle = characterNormal;
-		blockOne.fillRect(posX, posY, size, size);
+		context.fillStyle = collisionBlock;
+		context.fillRect(collisionBlockX, collisionBlockY, size, size);
+		context.fillStyle = normalColor;
+		context.fillRect(posX, posY, size, size);
 	}
 
 	// clear previous character to default (prevent bugs)
 	function clearCanvas() {
-		blockOne.clearRect(0, 0, canvas.width, canvas.height);
+		context.clearRect(0, 0, canvas.width, canvas.height);
 	}
 
 	// start the game
 	function gameLoop() {
 		clearCanvas();
+		collide();
 		updateCharacterPosition();
 		renderCharacter();
 		window.requestAnimationFrame(gameLoop);
